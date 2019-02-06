35ad62c261f38034eac80453f0a4ebc76111cedc
Cagatay Sert <cagatay.sert5@gmail.com>
2019-02-06 17:34:26 +0300

    redirect functionality is added to question circle(not server side , only client side)

---
+++ QuizApp.MVC/Controllers/AnswerController.cs
@@ -0,0 +1,30 @@
+﻿using QuizApp.Interfaces.AnswerService;
+using QuizApp.Interfaces.QuestionService;
+using QuizzApp.Entities.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace QuizApp.MVC.Controllers
+{
+    public class AnswerController : Controller
+    {
+        IAnswerService _answerService;
+        IQuestionService _questionService;
+        public AnswerController(IAnswerService answerService,IQuestionService questionService)
+        {
+            _answerService = answerService;
+            _questionService = questionService;
+        }
+
+        // GET: Answer
+        public ActionResult GetAnswersAndQuestion(int choosenAnswerId)
+        {
+            Answer answer = _answerService.GetById(s => s.Id == choosenAnswerId);
+            QuestionWithAnswers questionWithAnswers = _questionService.GetQuestionWithAnswersById(answer.QuestionId);
+            return View(questionWithAnswers);
+        }
+    }
+}
